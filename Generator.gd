d7c6a21e329911ece86cdce391c924fdb85ce501
cfdj <callumderry@gmail.com>
2024-01-20 13:53:08 +0000

    Given up on 3 levels
    Given up on 3 levels, probably needs more tiles

--- Generator.gd
+++ Generator.gd
@@ -10,15 +10,16 @@ func _ready():
 func generate():
 	get_tree().call_group("Tiles","generate")
 	var checking = tileLocations.keys()
-	for i in tileMap.get_layers_count()-1:
+	var terrains = tileMap.tile_set.get_terrains_count(0);
+	for i in terrains:
 		var tiles = [];
 		for x in checking:
 			if(tileLocations[x] == i):
 				tiles.append(x);
-		tileMap.set_cells_terrain_connect(i,tiles,0,0,true)
+		tileMap.set_cells_terrain_connect(1,tiles,0,i,true)
 	
 
 func placeTile(layer:int,location:Vector2i,tile:Vector2i,terrainLevel:int):
 	##tileMap.set_cell(layer,location,0,tile);
-	tileLocations[location] = layer
+	tileLocations[location] = terrainLevel
 	tileMap.erase_cell(2,location);
