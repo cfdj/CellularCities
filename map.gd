0a9a6d5b9d7de15f6941e55ba7141a6f1fcac122
cfdj <callumderry@gmail.com>
2024-01-21 22:34:18 +0000

    Flat upgrading now working

--- map.gd
+++ map.gd
@@ -5,7 +5,7 @@ var currentFoundries:Dictionary;
 var currentOffices:Dictionary;
 var currentAmmenities:Dictionary;
 var currentEmptyTiles:Dictionary;
-
+var currentFlats:Dictionary;
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Singletons.tilemap = self;
@@ -26,8 +26,18 @@ func addFoundry(location:Vector2i,foundry:Foundry):
 func removeFoundry(location:Vector2i,foundry:Foundry):
 	currentFoundries.erase(location);
 	set_cell(0,location,1,Vector2i(0,0),0)
+func addFlat(location:Vector2i,flat:Flat):
+	currentEmptyTiles.erase(location);
+	currentFlats[location] = flat;
+func removeFlat(location:Vector2i,flat:Flat):
+	currentFlats.erase(location);
+	set_cell(0,location,1,Vector2i(0,0),0);
+func upgradeToFlat(location:Vector2i,tileID:int):
+	currentStreets.erase(location);
+	set_cell(0,location,1,Vector2i(0,0),tileID)
 func addEmptyTile(location:Vector2i,tile:Tile):
 	currentEmptyTiles[location] = tile;
+
 func getHighestDesireEmpty() -> Vector2i:
 	var newLocation:Vector2i;
 	var highest = 0;
