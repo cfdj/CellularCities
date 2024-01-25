1c8180097480a483ecee45fd9e743be31fcb9d41
cfdj <callumderry@gmail.com>
2024-01-25 15:42:11 +0000

    Some Buildings added

--- Generator/BuildingTypes/Foundry.gd
+++ Generator/BuildingTypes/Foundry.gd
@@ -3,12 +3,13 @@ class_name Foundry extends Tile
 @export var currentEmployment:int;
 @export var maximumEmployment:int;
 var inRange:Dictionary;
+@export var alternativeFrames:Array[Vector2i];
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	super._ready();
 	map.addFoundry(location,self);
 	add_to_group("Foundries");
-
+	$Sprite2D.frame_coords = alternativeFrames[randi() % alternativeFrames.size()]
 func move(newLocation:Vector2i):
 	map.set_cell(0,newLocation,1,Vector2i(0,0),tileID)
 	map.removeFoundry(location,self);
